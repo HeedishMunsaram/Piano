6ce9c3ee9079cef4e01e2cda8a7e011141516ea5
Heedish Munsaram <54936388+HeedishMunsaram@users.noreply.github.com>
2019-09-27 14:34:05 +0400

    Add files via upload

---
+++ keys.java
@@ -0,0 +1,13 @@
+
+public class keys {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int keys = 88;
+		System.out.println("A piano has " + keys + " keys.");
+		
+	
+		
+	}
+
+}
